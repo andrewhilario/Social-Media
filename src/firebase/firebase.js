a37f8e6498fc1e1da2b764f4847144d8c46dd7dd
Drew Hilario <hilarioandrew12@gmail.com>
2023-10-08 10:31:11 +0800

    Change the firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOtLqyC3TBquSYo5DzAghhSRtzqgDcvAo",
-  authDomain: "paysbook-backend.firebaseapp.com",
-  projectId: "paysbook-backend",
-  storageBucket: "paysbook-backend.appspot.com",
-  messagingSenderId: "927173840597",
-  appId: "1:927173840597:web:f1ef06e8962d5d52c7dc5c"
+  apiKey: "AIzaSyDMey203nIlWeaNES9bt6wG7MQy9O5ZREg",
+  authDomain: "paysbook-social.firebaseapp.com",
+  projectId: "paysbook-social",
+  storageBucket: "paysbook-social.appspot.com",
+  messagingSenderId: "240526928699",
+  appId: "1:240526928699:web:d1154f43b7d44a4623e51e"
 };
 
 const app = initializeApp(firebaseConfig);
